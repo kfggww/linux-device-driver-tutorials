5156ce8462d7bc72ca9d5185642aeda732394aec
kfggww <kfggww@outlook.com>
2023-07-12 16:09:02 +0800

    Add hello_world driver.

--- Makefile
+++ Makefile
@@ -1,10 +1,29 @@
 ARCH ?= riscv
 CROSS_COMPILE ?= riscv64-linux-gnu-
+
+QEMU = qemu-system-riscv64
+QEMU_OPTIONS += -machine virt \
+				-smp 4 \
+				-kernel $(shell find third-party -maxdepth 1 -name linux*)/build/arch/$(ARCH)/boot/Image \
+				-drive file=$(shell pwd)/third-party/rootfs.ext4,format=raw,id=hd0 \
+				-device virtio-blk-device,drive=hd0 \
+				-fsdev local,id=lddt,path=$(shell pwd),security_model=passthrough \
+				-device virtio-9p-pci,fsdev=lddt,mount_tag=lddtsrc \
+				-append "root=/dev/vda ro console=ttyS0 init=/bin/init.sh" \
+				-nographic
+
 Q := @
 ifeq ($(V), 1)
 	Q :=
 endif
 
-.PHONY: prepare-third-party
 prepare-third-party:
 	$(Q)ARCH=$(ARCH) CROSS_COMPILE=$(CROSS_COMPILE) scripts/prepare-third-party.sh
+
+qemu_debug:
+	$(Q)$(QEMU) $(QEMU_OPTIONS) -s -S
+
+qemu_run:
+	$(Q)$(QEMU) $(QEMU_OPTIONS)
+
+.PHONY: prepare-third-party qemu_debug qemu_run
--- scripts/prepare-third-party.sh
+++ scripts/prepare-third-party.sh
@@ -18,22 +18,80 @@ download_and_extract_tarballs() {
 
     pushd $third_party_dir/.tmp
 
-    wget $kernel_url
-    wget $busybox_url
+    echo "Start downloading tarballs..."
+    if [ ! -e linux-${kernel_version}.tar.xz ]; then
+        wget $kernel_url
+    fi
+    if [ ! -e busybox-${busybox_version}.tar.bz2 ]; then
+        wget $busybox_url
+    fi
+    echo "End downloading tarballs"
 
-    local kenel_tarball=$(find . -name linux*.tar.xz)
+    local kernel_tarball=$(find . -name linux*.tar.xz)
     local busybox_tarball=$(find . -name busybox*.tar.bz2)
 
-    tar -xf $kernel_tarball -C ..
-    tar -xf $busybox_tarball -C ..
+    echo "Start extracting tarballs..."
+    if [ ! -e ../linux-${kernel_version} ]; then
+        tar -xf $kernel_tarball -C ..
+    fi
+
+    if [ ! -e ../busybox-${busybox_version} ]; then
+        tar -xf $busybox_tarball -C ..
+    fi
+    echo "End extracting tarballs"
 
     popd
 }
 
 build_kernel() {
-    echo pass
+    pushd ${third_party_dir}/linux-${kernel_version}
+    ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make O=build defconfig
+    cp build/.config build/.config.old
+    echo "CONFIG_DEBUG_INFO=y" >>build/.config
+    echo "CONFIG_GDB_SCRIPTS=y" >>build/.config
+    ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make O=build -j$(nproc)
+    popd
 }
 
 build_busybox() {
-    echo pass
+    pushd ${third_party_dir}/busybox-${busybox_version}
+    ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make defconfig
+    cp .config .config.old
+    echo "CONFIG_STATIC=y" >>.config
+    popd
 }
+
+build_rootfs() {
+    pushd ${third_party_dir}/busybox-${busybox_version}
+    ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} make install
+    dd if=/dev/zero of=rootfs.ext4 bs=4M count=32
+    mkfs.ext4 rootfs.ext4
+    mkdir -p mnt
+    guestmount -a rootfs.ext4 -m /dev/sda mnt
+
+    cp -r _install/* mnt
+    mkdir -p mnt/mnt
+    mkdir -p /dev
+    mkdir -p /sys
+    mkdir -p /proc
+
+    touch mnt/bin/init.sh
+    chmod +x mnt/bin/init.sh
+
+    cat <<EOF >mnt/bin/init.sh
+#!/bin/sh
+mount -t sysfs none /sys
+mount -t proc none /proc
+mount -t 9p lddtsrc /mnt
+
+/bin/sh
+EOF
+    guestunmount mnt
+    mv rootfs.ext4 ..
+    popd
+}
+
+download_and_extract_tarballs
+build_kernel
+build_busybox
+build_rootfs
